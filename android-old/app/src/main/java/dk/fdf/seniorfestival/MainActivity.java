0e0de3e2ff13c126100e9e22d7417515ed6f7994
Martin René Plett Pedersen <martin@fdf.dk>
2026-08-27 06:57:41 +0200

    Upgrades packages

---
+++ android-old/app/src/main/java/dk/fdf/seniorfestival/MainActivity.java
@@ -0,0 +1,5 @@
+package dk.fdf.seniorfestival.g2;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- android/app/src/main/java/dk/fdf/seniorfestival/MainActivity.java
+++ android/app/src/main/java/dk/fdf/seniorfestival/MainActivity.java
@@ -1,5 +1,54 @@
-package dk.fdf.seniorfestival.g2;
+package dk.fdf.seniorfestival;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.webkit.PermissionRequest;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebChromeClient;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1001;
+    private PermissionRequest pendingWebViewPermissionRequest;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        this.bridge.getWebView().setWebChromeClient(new BridgeWebChromeClient(this.bridge) {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    request.grant(request.getResources());
+                    return;
+                }
+
+                pendingWebViewPermissionRequest = request;
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[] { Manifest.permission.CAMERA },
+                        CAMERA_PERMISSION_REQUEST_CODE
+                );
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode != CAMERA_PERMISSION_REQUEST_CODE || pendingWebViewPermissionRequest == null) {
+            return;
+        }
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            pendingWebViewPermissionRequest.grant(pendingWebViewPermissionRequest.getResources());
+        } else {
+            pendingWebViewPermissionRequest.deny();
+        }
+
+        pendingWebViewPermissionRequest = null;
+    }
+}
